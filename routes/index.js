1771c2978052a09793a06c33c3a83c2d7585cdac
pstalcup <legomaniac0117@gmail.com>
2013-12-29 00:40:14 -0500

    Updated some stuff

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,14 @@
  */
 
 exports.index = function(req, res){
+    console.log(req.session); 
   res.render('index', { username: req.session.username });
+};
+
+exports.login = function(req, res){
+    var post = req.body; 
+    if(true) {
+        req.session.username = post.username; 
+    }
+    res.send("{}"); 
 };
